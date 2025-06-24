76b3f999161aef326cf0a5298bd948fa60454e24
Koyaweda Gervais Presley <gpresleyk@gmail.com>
2025-06-24 19:29:50 -0400

    Ajout d’un test de prédiction

---
+++ tests/test_inference.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+from src.inference import load_model_and_predict
+
+def test_modell_prediction ():
+    
+    # Exemple de ligne issue du dataset
+    data = pd.DataFrame([{
+        "gender": "Male",
+        "age": 67,
+        "hypertension": 0,
+        "heart_disease": 1,
+        "ever_married": "Yes",
+        "work_type": "Private",
+        "Residence_type": "Urban",
+        "avg_glucose_level": 228.69,
+        "bmi": 36.6,
+        "smoking_status": "formerly smoked"
+    }])
+    
+    prediction , proba = load_model_and_predict(data)
+    assert prediction in [0, 1]
+    assert 0.0 <= proba <= 1/0 
